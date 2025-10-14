08562967b93daeb1cecd1eb9c9df323e07252a17
AliGad17611 <aboali.almasri.2017@gmail.com>
2025-10-15 00:21:23 +0300

    - add widget testing to check what show in the screen and what appear if error data entered and valid data entered

--- test/widgets/user_registration_form_test.dart
+++ test/widgets/user_registration_form_test.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_testing_lab/widgets/user_registration_form.dart';
 
 void main() {
-  group('User Registration Form', () {
+  group('Unit Tests for validation logic', () {
     test('isValidEmail', () {
       expect(UserRegistrationForm.isValidEmail('test@test.com'), isTrue);
       expect(UserRegistrationForm.isValidEmail('test@test'), isFalse);
@@ -16,6 +17,45 @@ void main() {
       expect(UserRegistrationForm.isValidPassword('Password123'), isFalse);
       expect(UserRegistrationForm.isValidPassword('password123!'), isFalse);
       expect(UserRegistrationForm.isValidPassword('password123!'), isFalse);
+    });
+  });
+  group('Widget Tests for form submission', () {
+    late UserRegistrationForm form;
+    setUp(() {
+      form = UserRegistrationForm();
+    });
+    testWidgets('should validate form fields', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(home: Scaffold(body: form)));
+      expect(find.text('Full Name'), findsOneWidget);
+      expect(find.text('Email'), findsOneWidget);
+      expect(find.text('Password'), findsOneWidget);
+      expect(find.text('Confirm Password'), findsOneWidget);
+    });
+    testWidgets('should show Registration successful! when form is submitted with valid fields', (
+      WidgetTester tester,
+    ) async {
+      await tester.pumpWidget(MaterialApp(home: Scaffold(body: form)));
+
+      await tester.enterText(find.byType(TextFormField).first, 'John Doe');
+      await tester.enterText(
+        find.byType(TextFormField).at(1),
+        'john.doe@example.com',
+      );
+      await tester.enterText(find.byType(TextFormField).at(2), 'Password123!');
+      await tester.enterText(find.byType(TextFormField).at(3), 'Password123!');
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+      expect(find.text('Registration successful!'), findsOneWidget);
+    });
+    testWidgets('should show Please correct the errors in the form. when form is submitted with invalid fields', (WidgetTester tester) async {
+      await tester.pumpWidget(MaterialApp(home: Scaffold(body: form)));
+      await tester.enterText(find.byType(TextFormField).first, 'John Doe');
+      await tester.enterText(find.byType(TextFormField).at(1), 'john.doe@example.com');
+      await tester.enterText(find.byType(TextFormField).at(2), 'Password123');
+      await tester.enterText(find.byType(TextFormField).at(3), 'Password123!');
+      await tester.tap(find.byType(ElevatedButton));
+      await tester.pumpAndSettle();
+      expect(find.text('Please correct the errors in the form.'), findsOneWidget);
     });
   });
 }
